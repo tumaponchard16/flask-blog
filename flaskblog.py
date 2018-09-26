0c66e17ff5ad230502ee227e1891e7c31534935e
Richard <tumaponchard16@gmail.com>
2018-09-26 22:32:23 +0800

    Forms and User input with flask-wtf

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dbbcb0b80858d417a39c0ee0d44d7d78'
 
 # Dummy data
 posts = [
@@ -31,6 +34,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+# Registration route
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+# Login Route
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
