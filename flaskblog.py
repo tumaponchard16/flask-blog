8c3941cbdef04accd3aff91720ed4b0f8a8715f9
Richard <tumaponchard16@gmail.com>
2018-09-26 17:36:27 +0800

    Template inheritance and bootstrap added

--- flaskblog.py
+++ flaskblog.py
@@ -30,7 +30,7 @@ def home():
 # About page
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
