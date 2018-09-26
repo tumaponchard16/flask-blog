09e4d45f1b0f45ccd0117dd07e0c128e7fbe7798
Richard <tumaponchard16@gmail.com>
2018-09-26 17:03:56 +0800

    Templating and passing data to template

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Dummy data
+posts = [
+    {
+        'author': 'Richard Tumapon',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'September 25, 2018'
+    },
+
+    {
+        'author': 'John Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 26, 2018'
+    }
+]
 
 
 # Get homepage
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Hello World!"
+    return render_template('home.html', posts=posts)
 
 
 # About page
 @app.route("/about")
 def about():
-    return "<h1>About page!</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
