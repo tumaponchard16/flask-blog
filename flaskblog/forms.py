5f681c6d8151944aa5f8e0e3465bfff51eca245a
Richard <tumaponchard16@gmail.com>
2018-10-02 23:49:34 +0800

    Routes, forms and templates added for Post

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from flaskblog.models import User
@@ -56,3 +56,9 @@ class UpdateAccountForm(Form):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is already taken. Please choose different one.')
+
+
+class PostForm(Form):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
